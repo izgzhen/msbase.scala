4fcb0df5cfc96d476bd76da6834bf75453c85897
Zhen Zhang <git@zhen-zhang.com>
2020-02-28 10:23:40 -0800

    Order key

--- src/main/scala/io/github/izgzhen/msbase/JsonUtil.scala
+++ src/main/scala/io/github/izgzhen/msbase/JsonUtil.scala
@@ -1,12 +1,13 @@
 package io.github.izgzhen.msbase
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.{ObjectMapper, SerializationFeature}
 import com.fasterxml.jackson.module.scala.{DefaultScalaModule, ScalaObjectMapper}
 
 /* Created at 2/26/20 by zhen */
 object JsonUtil {
   val mapper = new ObjectMapper() with ScalaObjectMapper
   mapper.registerModule(DefaultScalaModule)
+  mapper.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true)
 
   def toJson(value: Any): String = {
     mapper.writerWithDefaultPrettyPrinter.writeValueAsString(value)
