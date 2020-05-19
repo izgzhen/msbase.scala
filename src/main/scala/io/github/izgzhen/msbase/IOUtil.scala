044c37fb6040ce7c3e0254fb39ca09701300514d
Zhen Zhang <git@zhen-zhang.com>
2020-05-19 10:40:17 -0700

    Add more IOUtils

--- src/main/scala/io/github/izgzhen/msbase/IOUtil.scala
+++ src/main/scala/io/github/izgzhen/msbase/IOUtil.scala
@@ -2,12 +2,25 @@ package io.github.izgzhen.msbase
 
 import java.io.{BufferedWriter, File, FileWriter}
 
+import scala.jdk.CollectionConverters._
+
 /* Created at 5/3/20 by zhen */
 object IOUtil {
   def writeLines(lines: List[String], dest: String): Unit = {
     val file = new File(dest)
     val bw = new BufferedWriter(new FileWriter(file))
     bw.write(lines.mkString("\n"))
+    bw.close()
+  }
+
+  def writeLinesJava(lines: java.util.List[String], dest: String): Unit = {
+    writeLines(lines.asScala.toList, dest)
+  }
+
+  def write(s: String, dest: String): Unit = {
+    val file = new File(dest)
+    val bw = new BufferedWriter(new FileWriter(file))
+    bw.write(s)
     bw.close()
   }
 }
